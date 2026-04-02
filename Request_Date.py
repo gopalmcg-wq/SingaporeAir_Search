8448f9d76ef3b0a81627e736ffd53bb148102ebc
gopalmcg-wq <gopalmcg@gmail.com>
2026-04-02 00:06:47 -0700

    Update Request_Date.py

--- Request_Date.py
+++ Request_Date.py
@@ -6,7 +6,7 @@ def searchSite(payload):
 #  payload = "{\r\n\t\"clientUUID\":\"SQ-API-Booking-Aggregator\",\r\n\t\"request\":{\r\n   \t  \t\"itineraryDetails\":[  \r\n   \t  \t\t{  \r\n\t            \"originAirportCode\":\"SIN\",\r\n\t            \"destinationAirportCode\":\"PER\",\r\n\t\t        \"departureDate\":\"2020-10-04\",\r\n\t\t        \"returnDate\":\"2020-10-04\"\r\n\t         }\r\n\t    ],\r\n\t    \"cabinClass\":\"Y\",\r\n\t    \"adultCount\":1,\r\n\t    \"childCount\":0,\r\n\t    \"infantCount\":0\r\n\t}\r\n,\r\n\t    \"flexibleDates\":\"false\"}"
   headers = {
     'Content-Type': 'application/json',
-    'apikey': '<insert own API here>'
+    'apikey': 'px8cqzuw95zhgdqv6kucjzuukqn5ekbb7nz5hg75m35pzuz5'
   }
 
   response = requests.request("POST", url, headers=headers, data = payload)
